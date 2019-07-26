dca45c43bc0e03efc53f8eb2b30c52e27f9855a7
David Alfageme <alfalfageme@gmail.com>
2019-07-26 09:22:53 +0200

    Primera aproximación borrado

--- 20190725/dom/main.js
+++ 20190725/dom/main.js
@@ -9,10 +9,13 @@ window.addEventListener('load', () => {
 
   var searchButton = document.querySelector('#searchButton');
   searchButton.addEventListener('click', search);
+
+  var deleteButton = document.querySelector('#deleteButton');
+  deleteButton.addEventListener('click', deleteItem);
 });
 
 // Añadir el contenido del input a la lista
-function addItem(){
+function addItem() {
   let productInput = document.querySelector('#productInput');
   let productName = productInput.value; // Obtiene el texto del input
   compra.push(productName); // añade a la lista de la compra
@@ -22,24 +25,44 @@ function addItem(){
 
 // Se encarga de imprimir los productos
 // elements to print toma compra como parámetro por defecto
-function printElements(elementsToPrint = compra){
+function printElements(elementsToPrint = compra) {
   var lista = document.getElementById('lista');
   lista.innerHTML = ""; // borra todo lo que habia antes
-  elementsToPrint.forEach(function(producto){
+  elementsToPrint.forEach(function (producto) {
     var liElement = document.createElement('li');
     liElement.innerText = producto;
     lista.appendChild(liElement); // añade el elemento al final de la lista
   })
 }
 
-function search(){
+function search() {
   var searchInput = document.querySelector("#searchInput");
   var searchText = searchInput.value;
 
-  var resultadosBusqueda = compra.filter(function(producto){
+  var resultadosBusqueda = compra.filter(function (producto) {
     //Convertimos producto y búsqueda a minúsculas
     return producto.toLowerCase().includes(searchText.toLowerCase());
   });
 
   printElements(resultadosBusqueda);
+}
+
+
+function deleteItem(){
+  var deleteInput = document.querySelector("#deleteInput");
+  var deleteText = deleteInput.value;
+
+  // array findIndex
+  var indiceBorrar = compra.findIndex(function(producto){
+    return producto.toUpperCase() === deleteText.toUpperCase();
+  });
+
+  // splice 
+  compra.splice(indiceBorrar, 1);
+
+  // compra = compra.filter(function(producto) {
+  //   return !producto.toLowerCase().includes(deleteText.toLowerCase());
+  // });
+
+  printElements();
 }
